4fc7f1eef12ec0a99a63cdeeea3569b1d1a70ef6
Paul+Andrew <Paul+Andrew@appacademy.io>
2015-01-28 15:47:11 -0500

    Add move converstion (ex: a3 => [5,0])

--- human_player.rb
+++ human_player.rb
@@ -1,5 +1,24 @@
 class HumanPlayer
 
+  POSITION_CONVERSION = {
+    "A" => 0,
+    "B" => 1,
+    "C" => 2,
+    "D" => 3,
+    "E" => 4,
+    "F" => 5,
+    "G" => 6,
+    "H" => 7,
+    "8" => 0,
+    "7" => 1,
+    "6" => 2,
+    "5" => 3,
+    "4" => 4,
+    "3" => 5,
+    "2" => 6,
+    "1" => 7,
+  }
+
   attr_accessor :color
 
   def initialize(color)
@@ -8,14 +27,16 @@ class HumanPlayer
 
   def play_turn
     puts "Enter start position."
-    start_pos = gets.chomp
+    start_pos = gets.chomp.upcase.split('')
     puts "Enter end position."
-    end_pos = gets.chomp
+    end_pos = gets.chomp.upcase.split('')
 
-    start_pos = start_pos.split(",").map { |num| num.to_i }
-    end_pos = end_pos.split(",").map { |num| num.to_i }
+    start_pos.map! { |i| POSITION_CONVERSION[i] }
+    end_pos.map! { |i| POSITION_CONVERSION[i] }
+    # start_pos = start_pos.split(",").map { |num| num.to_i }
+    # end_pos = end_pos.split(",").map { |num| num.to_i }
 
-    [start_pos, end_pos]
+    [start_pos.reverse, end_pos.reverse]
   end
 
 end
